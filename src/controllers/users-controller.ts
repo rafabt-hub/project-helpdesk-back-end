4b7c059a9d07e2ff27a6bf1339658cd945a4820e
Rafael <rafaelbthomaz@gmail.com>
2025-06-06 14:13:41 -0300

    sessions controller

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -32,7 +32,7 @@ class UsersController {
 
     const { password: _, ...userWithoutPassword } = user
 
-    return response.json(userWithoutPassword)
+    return response.status(201).json(userWithoutPassword)
   }
 }
 
