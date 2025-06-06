0fd40effcbc1186c6b3449d2335217a93f165eba
Rafael <rafaelbthomaz@gmail.com>
2025-06-06 14:46:05 -0300

    user verifcation on tickets

--- src/routes/tickets-routes.ts
+++ src/routes/tickets-routes.ts
@@ -3,11 +3,12 @@ import { Router } from "express"
 import { TicketsController } from "@/controllers/tickets-controller"
 
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated"
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization"
 
 const ticketsRoutes = Router()
 const ticketsController = new TicketsController()
 
-ticketsRoutes.use(ensureAuthenticated)
+ticketsRoutes.use(ensureAuthenticated, verifyUserAuthorization(["admin", "client"]))
 ticketsRoutes.post("/", ticketsController.create)
 
 export { ticketsRoutes }
