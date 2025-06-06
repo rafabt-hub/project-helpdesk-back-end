8ac4b97fe94041d475d8d0b7b90e774c44cdb4f3
Rafael <rafaelbthomaz@gmail.com>
2025-06-05 21:40:34 -0300

    validation users

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,38 @@
 import { Request, Response } from "express"
+import { AppError } from "@/utils/AppError"
+import { prisma } from "@/database/prisma"
+import { hash } from "bcrypt"
+import { z } from "zod"
 
 class UsersController {
-  create(request: Request, response: Response) {
-    return response.json({ message: "ok"})
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(3),
+      email: z.string().email(),
+      password: z.string().min(6),
+    })
+
+    const { name, email, password } = bodySchema.parse(request.body)
+
+    const userWithSameEmail = await prisma.user.findFirst({ where: { email }})
+
+    if (userWithSameEmail) {
+      throw new AppError("User with same email already exists")
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+
+    const { password: _, ...userWithoutPassword } = user
+
+    return response.json(userWithoutPassword)
   }
 }
 
