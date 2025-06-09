ee0125801ab17866c162b734f3c1abe36686645f
Rafael <rafaelbthomaz@gmail.com>
2025-06-09 16:25:55 -0300

    adding uploads routes on index routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import { usersRoutes } from "./users-routes"
 import { sessionsRoutes } from "./sessions-routes"
 import { servicesRoutes } from "./services-routes"
 import { ticketsRoutes } from "./tickets-routes"
+import { uploadsRoutes } from "./uploads-routes"
 
 
 const routes = Router()
@@ -11,5 +12,6 @@ routes.use("/users", usersRoutes)
 routes.use("/sessions", sessionsRoutes)
 routes.use("/tickets", ticketsRoutes)
 routes.use("/services", servicesRoutes)
+routes.use("/uploads", uploadsRoutes)
 
 export { routes }
