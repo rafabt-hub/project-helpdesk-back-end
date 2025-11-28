ba2859d23c5e667c40b80ef5715ca0d17e7def4f
Rafael <rafaelbthomaz@gmail.com>
2025-11-28 12:17:29 -0300

    fix on comunication front end

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import "express-async-errors"
 import * as path from "path"
+import cors from "cors"
 
 import { routes } from "./routes"
 import { errorHandling } from "./middlewares/error-handling"
@@ -8,9 +9,11 @@ import { uploadsRoutes } from "./routes/uploads-routes"
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use("/uploads", uploadsRoutes)
+
 
 app.use(errorHandling)
 
